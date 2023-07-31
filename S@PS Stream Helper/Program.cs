88142f63619b33ea5d150cb9a7e615c0a467af21
Aryan Sinha <aryananusinha@gmail.com>
2023-07-30 22:31:38 -0400

    Added the ability to get info about a tournament from a slug link

--- S@PS Stream Helper/Program.cs
+++ S@PS Stream Helper/Program.cs
@@ -1,7 +1,61 @@
-﻿public class Program
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+public class Program
 {
-    public static void Main(string[] args)
+    private static HttpClient s_Client = new HttpClient();
+
+    private struct Tournament
     {
-        Console.WriteLine("Hello World");
+        public int id;
+        public string? name;
+    }
+
+    public static async Task Main(string[] args)
+    {
+        const string endpoint = "https://api.start.gg/gql/alpha";
+        Console.Write("Please input your auth token: ");
+        string? authToken = Console.ReadLine();
+
+        Console.Write("Please input your desired slug: ");
+        string? slug = Console.ReadLine();
+
+        const string query = @"
+            query TournamentName($slug: String!)
+            {
+                tournament(slug: $slug)
+                {
+                    id
+                    name
+                }
+            }
+        ";
+
+        var variables = new
+        {
+            slug = slug
+        };
+
+        s_Client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authToken}");
+
+        var request = new
+        {
+            query,
+            variables
+        };
+
+        string jsonRequest = JsonSerializer.Serialize(request);
+        StringContent content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+        HttpResponseMessage response = await s_Client.PostAsync(endpoint, content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            JsonNode jsonQuery = JsonNode.Parse(jsonResponse);
+            JsonNode data = jsonQuery["data"];
+            Console.WriteLine(data);
+        }
     }
 }
